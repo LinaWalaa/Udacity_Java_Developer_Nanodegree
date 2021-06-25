037c7170fdaaa6d47e4cafee94a6486b7ac44a7d
Lina Walaa <linawalaa95@gmail.com>
2021-06-25 04:33:28 +0200

    Update redirection to the login upon logout

--- 2- Spring Boot Basics/Lesson 5 Data Persistence & Security/5- Final Review/src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
+++ 2- Spring Boot Basics/Lesson 5 Data Persistence & Security/5- Final Review/src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
@@ -30,8 +30,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//       In order for us to redirect the user to the login page 
+//       without using a Controller first we have to add 
+//       the "/login" to .antMatchers so that we are able to 
+//       send the logout parameter and then we will remove 
+//       the .logoutUrl("/logout") and in .logoutSuccessUrl 
+//       we will put the logout as parameter
         http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**", "/h2").permitAll()
+                .antMatchers("/login","/signup", "/css/**", "/js/**", "/h2").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
@@ -41,16 +47,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .defaultSuccessUrl("/chat", true);
 
+        //The logoutRequestMatcher sets the endpoint where Spring
+        // Security will receive a request to log a user out.
+        //And its use is important because it allows us, even with
+        // csrf enabled, to log the user out through an HTTP GET request.
+
         http.logout()
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login");
-//                .permitAll()
-//                .logoutSuccessUrl("/login?logout");
-
-//        http.csrf().disable();
+                .logoutSuccessUrl("/login?logout");
     }
 
 
