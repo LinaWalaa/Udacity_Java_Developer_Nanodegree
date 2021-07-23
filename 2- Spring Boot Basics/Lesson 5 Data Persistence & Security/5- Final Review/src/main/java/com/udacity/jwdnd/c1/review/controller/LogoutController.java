0e578b886b4b850b54ddfa708d3fec69fe9beae2
Lina Walaa <linawalaa95@gmail.com>
2021-07-23 14:51:54 +0200

    comment out everything on the logout controller
    Since upon pressing the logout button we're redirecting the user to the login page, then there is no need for the logout.html and accordingly to the logout controller
    
    I had previously added some important comments that I want to keep for reference regarding the difference between post and get methods

--- 2- Spring Boot Basics/Lesson 5 Data Persistence & Security/5- Final Review/src/main/java/com/udacity/jwdnd/c1/review/controller/LogoutController.java
+++ 2- Spring Boot Basics/Lesson 5 Data Persistence & Security/5- Final Review/src/main/java/com/udacity/jwdnd/c1/review/controller/LogoutController.java
@@ -1,17 +1,32 @@
-package com.udacity.jwdnd.c1.review.controller;
-
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
-@Controller
-@RequestMapping("/logout")
-public class LogoutController {
-
-    @GetMapping()
-    public String logout(){
-       return "logout";
-    }
-}
+//package com.udacity.jwdnd.c1.review.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/logout")
+//public class LogoutController {
+//
+//    //We used POST on the chat.html page
+//    // because GET logout is not recommended but the logout
+//    // through the POST method is.
+//
+//    //why not post mapping?
+//
+//    //1- we don't need to change any Controller annotations to
+//    // be post since this process will be managed by
+//    // Spring Security
+//    //2- Spring by default uses the post method
+//
+//    //3- since the authentication (login) process is being
+//    // done by Spring Security, then the logout also
+//    // has to be done, so putting an endpoint that will
+//    // receive a request regarding the logout makes no sense
+//    // since this process will be managed by Spring Security.
+//    @GetMapping()
+//    public String logout(){
+//        return "logout";
+//    }
+//}
